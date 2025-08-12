2a9dcd3a909ca224aebccede53f21806337d0f49
Dev_Ege <98964310+EgeDev003@users.noreply.github.com>
2025-08-12 19:10:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const PeakKey = "X9G7B4QW8L0FJ2RMK5YP3ZSA1HVT6CND"
+const WaitTime = 500
+const MaxTry = 5
 
 // Export our request handler
 export default {
@@ -7,27 +9,33 @@ export default {
         const path = url.pathname.split(/\//);
 
         if (path[1] === "gamepass") {
-            return new Response(JSON.stringify({ message: "yes"}), { status: 400 })
-        }
-        
-        const headers = new Headers(request.headers);
-        headers.delete("host");
-        headers.delete("roblox-id");
-        headers.delete("user-agent");
-        headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
-
-        console.log(headers.get("key"))
-        if (headers.get("key") !== PeakKey) {
-            return new Response(JSON.stringify({ message: "Key is not true"}), { status: 400 });
-        }
-        
-        const init = {
-            method: request.method,
-            headers,
-        };
-
-        if (request.method !== "GET" && request.method !== "HEAD") {
-            init.body = await request.text();
+            if (request.method === "GET") {
+                const headers = new Headers(request.headers);
+                
+                if (headers.get("key") !== PeakKey) {
+                    return new Response(JSON.stringify({ message: "Key is not true"}), { status: 400 });
+                }
+                
+                headers.delete("host");
+                headers.delete("roblox-id");
+                headers.delete("user-agent");
+                headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
+                
+                const init = {
+                    method: request.method,
+                    headers,
+                };
+                
+                function GetGamepass(cursor) {
+                    const Gamepasses = fetch("https://games.roblox.com/v1/games/" + path[2] + "/game-passes" + url.search)
+                    return Gamepasses
+                }
+                return GetGamepass()
+            } else {
+                return new Response(JSON.stringify({ message: "Wrong api method"}), { status: 400 })
+            }
+        } else {
+            return new Response(JSON.stringify({ message: "Undefined method"}), { status: 400 })
         }
     }
 };

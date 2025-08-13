ffad2448babc6ebb14f3913e53b1f09683b9baf8
Dev_Ege <98964310+EgeDev003@users.noreply.github.com>
2025-08-13 03:05:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ export default {
             } else {
                 return WrongApiErrorFunction();
             }
+        } else if(path[1] === "favicon.ico") {
+            return fetch("chrome://favicon2/?size=24&scaleFactor=1x&showFallbackMonogram=&pageUrl=https%3A%2F%2Fwww.youtube.com%2F%40sleitnick1")
         } else {
             return new Response(JSON.stringify({ message: "Undefined method"}), { status: 400 })
         }

6079dfa1c11142283878ea23d6e1f2c82f3adeb4
Dev_Ege <98964310+EgeDev003@users.noreply.github.com>
2025-08-14 23:01:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ export default {
                 };
                 
                 const ApiUrl = "https://games.roblox.com/v1/games/" + path[2] + "/game-passes" + url.search
-                return Gamepasses = fetch(ApiUrl)
+                return fetch(ApiUrl)
             } else {
                 return WrongApiErrorFunction(); 
             }

7652ad7a794762bc073363d051fd7b60a4b19632
Dev_Ege <98964310+EgeDev003@users.noreply.github.com>
2025-08-20 19:59:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -259,13 +259,14 @@ export default {
         headers.delete("user-agent");
         headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
 
-        const Body = await request.text();
+        const formData = await request.formData();
+        // const Body = await request.text();
 
-        if (!Body.trim()) {
-            console.log("sa")
-        } else {
-            console.log("as")
-        }
+        // if (!Body.trim()) {
+        //     console.log("sa")
+        // } else {
+        //     console.log("as")
+        // }
         
         if (path[1] === "deneme") {
             const form = new FormData();

3ffb64253295d60f42227de9b449ff872aac2004
Dev_Ege <98964310+EgeDev003@users.noreply.github.com>
2025-08-20 20:51:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -274,6 +274,7 @@ export default {
             form.append("UniverseId", headers.get("UniverseId"))
             form.append("Name", headers.get("Name"))
             form.append("Description", headers.get("Description"))
+            form.append("IsRegionalPricingEnabled", headers.get("IsRegionalPricingEnabled") == "true" and true or false)
             
             const init = {
                 method: "POST",
